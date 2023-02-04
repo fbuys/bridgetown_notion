025d13553f0122780b561a2823d4b9cd40bff3ab
Francois Buys <buys.fran@gmail.com>
2023-02-04 20:51:12 +0200

    🚧 Add basic content

--- lib/bridgetown-notion/builder.rb
+++ lib/bridgetown-notion/builder.rb
@@ -16,28 +16,56 @@ module BridgetownNotion
           tags = (post.dig("properties", "tags", "multi_select") || []).map { |t| t["name"] }.join(" ")
           is_published = post["properties"]["published"]["checkbox"]
           published_at = post.dig("properties", "published_at", "date", "start")
+          content = ""
 
-          if published_at
-            add_resource :posts, "#{published_at}-#{title}.md" do
-              author "rlstevenson"
-              categories categories
-              content "It's pretty _nifty_ that you can add **new blog posts** this way. <%= resource.data %>"
-              layout :post
-              published is_published
-              tags tags
-              title title
-            end
-            client.block_children(block_id: post.id) do |blocks_page|
-              blocks_page.results.each do |block|
-                # pp block
-              end
+          next if published_at.nil? || title.nil?
+
+          client.block_children(block_id: post.id) do |blocks_page|
+            blocks_page.results.each do |block|
+              content += extract_content(block)
             end
           end
-          Bridgetown.logger.info "################################"
+
+          add_resource :posts, "#{published_at}-#{title}.md" do
+            categories categories
+            content content
+            layout :post
+            published is_published
+            tags tags
+            title title
+          end
         end
       end
+    end
 
-      # Bridgetown.logger.info client
+    private
+
+    def extract_content(block)
+      result = block.dig("heading_1", "rich_text", 0, "text", "content")
+      return "# #{result}\n" if result
+
+      result = block.dig("heading_2", "rich_text", 0, "text", "content")
+      return "## #{result}\n" if result
+
+      result = block.dig("heading_3", "rich_text", 0, "text", "content")
+      return "### #{result}\n" if result
+
+      result = block.dig("heading_4", "rich_text", 0, "text", "content")
+      return "#### #{result}\n" if result
+
+      result = block.dig("heading_5", "rich_text", 0, "text", "content")
+      return "##### #{result}\n" if result
+
+      result = block.dig("heading_6", "rich_text", 0, "text", "content")
+      return "###### #{result}\n" if result
+
+      result = block.dig("paragraph", "rich_text", 0, "text", "content")
+      return "#{result}\n" if result
+
+      result = block.dig("bulleted_list_item", "rich_text", 0, "text", "content")
+      return "- #{result}\n" if result
+
+      "\n"
     end
   end
 end
