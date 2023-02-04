ee8c5792dc548f0d49252d038b9f72c6ef3092ad
Francois Buys <buys.fran@gmail.com>
2023-02-04 16:17:43 +0200

    🚧 Add tags and categories front matter

--- lib/bridgetown-notion/builder.rb
+++ lib/bridgetown-notion/builder.rb
@@ -12,14 +12,18 @@ module BridgetownNotion
       client.database_query(database_id: NOTION_DB_ID) do |posts_page|
         posts_page.results.each do |post|
           title = post["properties"]["Title"]["title"][0]["text"]["content"]
+          categories = (post.dig("properties", "categories", "multi_select") || []).map { |c| c["name"] }.join(" ")
+          tags = (post.dig("properties", "tags", "multi_select") || []).map { |t| t["name"] }.join(" ")
           is_published = post["properties"]["published"]["checkbox"]
 
           add_resource :posts, "2020-05-17-#{title}.md" do
-            layout :post
-            title title
-            published is_published
             author "rlstevenson"
+            categories categories
             content "It's pretty _nifty_ that you can add **new blog posts** this way. <%= resource.data %>"
+            layout :post
+            published is_published
+            tags tags
+            title title
           end
           client.block_children(block_id: post.id) do |blocks_page|
             blocks_page.results.each do |block|
