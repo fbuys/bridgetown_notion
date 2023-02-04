e46a744dfde34a009b6e80a81064299af1654c5a
Francois Buys <buys.fran@gmail.com>
2023-02-05 00:19:51 +0200

    🔧 Remove references to frontend files
    Also no need for a source manifest because we don't have frontend code.

--- lib/bridgetown_notion.rb
+++ lib/bridgetown_notion.rb
@@ -3,10 +3,3 @@
 require "bridgetown"
 require "bridgetown_notion/builder"
 require "notion-ruby-client"
-
-Bridgetown::PluginManager.new_source_manifest(
-  origin: BridgetownNotion,
-  components: File.expand_path("../components", __dir__),
-  content: File.expand_path("../content", __dir__),
-  layouts: File.expand_path("../layouts", __dir__)
-)
