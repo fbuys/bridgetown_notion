c18f910aaee80573c1c5b1d088db2cf06aea54ff
Francois Buys <buys.fran@gmail.com>
2023-02-04 12:09:41 +0200

    🚧 Copy and past playground implementation

--- lib/bridgetown-notion/builder.rb
+++ lib/bridgetown-notion/builder.rb
@@ -3,9 +3,31 @@
 module BridgetownNotion
   class Builder < Bridgetown::Builder
     def build
-      liquid_tag "sample_plugin" do
-        "This plugin works!"
+      Notion.configure do |config|
+        config.token = NOTION_KEY
       end
+
+      client = Notion::Client.new
+
+      client.database_query(database_id: NOTION_DB_ID) do |posts_page|
+        posts_page.results.each do |post|  
+          title = post['properties']['Title']['title'][0]['text']['content']
+          add_resource :posts, "2020-05-17-#{title}.md" do
+            layout :post
+            title "Way to Go, Bridgetown!"
+            author "rlstevenson"
+            content "It's pretty _nifty_ that you can add **new blog posts** this way."
+          end
+          client.block_children(block_id: post.id) do |blocks_page|
+            blocks_page.results.each do |block|  
+              pp block
+            end
+          end
+          puts '################################'
+        end
+      end
+
+      puts client
     end
   end
 end
